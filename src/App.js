1eca45072d8e43d9c17b75b626974e16c4afcea5
Josue Rodriguez <zhippuden@gmail.com>
2019-10-06 22:43:38 -0600

    Pages/MenuBar are ready and we now populate home page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-function App() {
-	return (
-		<div className='App'>
-			<h1>Hello World!</h1>
-		</div>
-	);
-}
+import MenuBar from './Components/MenuBar';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+
+const App = props => (
+	<Router>
+		<Container>
+			<MenuBar />
+			<Route exact path='/' component={Home} />
+			<Route exact path='/login' component={Login} />
+			<Route exact path='/register' component={Register} />
+		</Container>
+	</Router>
+);
 
 export default App;
